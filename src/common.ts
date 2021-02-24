7536812925319098f93a9cf676cfb0b6dd7f7276
urain39 <urain39@qq.com>
2021-02-25 00:04:09 +0800

    去除“aria2.”前缀。

--- src/common.ts
+++ src/common.ts
@@ -3,19 +3,19 @@ import { ARIA2ErrorCode } from './errorCode';
 export type ARIA2Optional<T> = { [key in keyof T]?: T[key] };
 
 export type ARIA2Method =
-    'aria2.addUri' | 'aria2.addTorrent' | 'aria2.getPeers' | 'aria2.addMetalink' | 'aria2.remove' |
-    'aria2.pause' | 'aria2.forcePause' | 'aria2.pauseAll' | 'aria2.forcePauseAll' | 'aria2.unpause' |
-    'aria2.unpauseAll' | 'aria2.forceRemove' | 'aria2.changePosition' | 'aria2.tellStatus' | 'aria2.getUris' |
-    'aria2.getFiles' | 'aria2.getServers' | 'aria2.tellActive' | 'aria2.tellWaiting' | 'aria2.tellStopped' |
-    'aria2.getOption' | 'aria2.changeUri' | 'aria2.changeOption' | 'aria2.getGlobalOption' | 'aria2.changeGlobalOption' |
-    'aria2.purgeDownloadResult' | 'aria2.removeDownloadResult' | 'aria2.getVersion' | 'aria2.getSessionInfo' |
-    'aria2.shutdown' | 'aria2.forceShutdown' | 'aria2.getGlobalStat' | 'aria2.saveSession' | 'system.multicall' |
+    'addUri' | 'addTorrent' | 'getPeers' | 'addMetalink' | 'remove' |
+    'pause' | 'forcePause' | 'pauseAll' | 'forcePauseAll' | 'unpause' |
+    'unpauseAll' | 'forceRemove' | 'changePosition' | 'tellStatus' | 'getUris' |
+    'getFiles' | 'getServers' | 'tellActive' | 'tellWaiting' | 'tellStopped' |
+    'getOption' | 'changeUri' | 'changeOption' | 'getGlobalOption' | 'changeGlobalOption' |
+    'purgeDownloadResult' | 'removeDownloadResult' | 'getVersion' | 'getSessionInfo' |
+    'shutdown' | 'forceShutdown' | 'getGlobalStat' | 'saveSession' | 'system.multicall' |
     'system.listMethods' | 'system.listNotifications';
 
 export type ARIA2NotifyMethod =
-    'aria2.onDownloadStart' | 'aria2.onDownloadPause' |
-    'aria2.onDownloadStop' | 'aria2.onDownloadComplete' |
-    'aria2.onDownloadError' | 'aria2.onBtDownloadComplete';
+    'onDownloadStart' | 'onDownloadPause' |
+    'onDownloadStop' | 'onDownloadComplete' |
+    'onDownloadError' | 'onBtDownloadComplete';
 
 export type ARIA2GID = string;
 
